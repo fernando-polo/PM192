aac83b7e443f0d2c39f201fcbe3fe5f8c69f4a56
fernando-polo <fernandogmzmdo@gmail.com>
2025-05-19 17:54:53 -0600

    Ejercicio 3 - c)
    Arrow Function con dos valores

---
+++ Practica1/c.js
@@ -0,0 +1,10 @@
+// Crea una arrow function llamada saludoPersonalizado que recibe dos 
+// parámetros: nombre y edad, y retorne una cadena como la siguiente
+
+// "Hola, me llamo Isay y tengo 37 años"
+
+let saludoPersonalizado = (nombre, edad) => {
+    return 'Hola, me llamo ' + nombre + ' y tengo ' + edad + ' años.'
+}
+
+console.log(saludoPersonalizado('fernando', 22))
