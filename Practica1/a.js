8c56abdd6713f56cc7da094744508dbfc3ea4f83
fernando-polo <fernandogmzmdo@gmail.com>
2025-05-19 17:40:56 -0600

    Ejercicio - a)
    Variables let y const

---
+++ Practica1/a.js
@@ -0,0 +1,14 @@
+// Corrige el siguiente código para que siga las buenas prácticas de Javascript moderno
+// (usa let y const en lugar de var) y asegúrate de que las variables no se puedan reasignar si no es necesario.
+
+let nombre = 'Armando';
+const edad = 25;
+
+nombre = 'Ana María';
+
+const saludo = 'Hola, ' + nombre + '. Tienes ' + edad + ' años';
+
+// muestra en consola en contenido de saludo
+console.log(saludo)
+
+
