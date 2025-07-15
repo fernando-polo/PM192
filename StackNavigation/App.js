31728950ef38ddd58fb7779cdcfd18a8d2f90197
fernando-polo <fernandogmzmdo@gmail.com>
2025-07-14 21:13:51 -0600

    PracticaNo.15:NavigationStack
    Se aprendió el uso de pilas con Navigation Stack

---
+++ StackNavigation/App.js
@@ -0,0 +1,33 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+
+import Home from './screens/home';
+import Profile from './screens/profile';
+import Settings from './screens/settings';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen 
+                    name="Home" 
+                    component={Home} 
+                    options={{ title: 'Inicio' }}
+                />
+                <Stack.Screen 
+                    name="Profile" 
+                    component={Profile} 
+                    options={{ title: 'Perfil' }}
+                />
+                <Stack.Screen 
+                    name="Settings" 
+                    component={Settings} 
+                    options={{ title: 'Configuración' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
